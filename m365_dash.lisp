b67e10afb6ff1a1b844c05c9b192baa375e75289
Zodiak1993 <96478454+Zodiak1993@users.noreply.github.com>
2025-08-26 20:16:15 +0200

    Update m365_dash.lisp

--- m365_dash.lisp
+++ m365_dash.lisp
@@ -1,6 +1,6 @@
 ; M365 dashboard compability lisp script
 ; UART Wiring: red=5V black=GND yellow=COM-TX (UART-HDX) green=COM-RX (button)+3.3V with 1K Resistor
-; Tested on VESC 6.05 using M365 BLE (version 1.3.6) with spintend ubox Lite 100 100
+; Tested on VESC 6.06 using M365 BLE (version 1.3.6) with spintend ubox Lite 100 100
 ; Edited by Zodiak: Thanks to AKA13, 1zuna and sharkboy for original script!
 
 ; ==============================================================================================================================
